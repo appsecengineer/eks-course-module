a8cfd84ff4f492eb74d758f5d847ffa34e5223a4
Kanumuru Rajesh <rajesh@we45.com>
2024-05-22 01:05:42 +0530

    changed the approach to enable the cloud logs using locals and variables

--- varibales.tf
+++ varibales.tf
@@ -13,8 +13,12 @@ variable "public_subnet_cidr2" {
   default = "10.0.1.0/24"
 }
 
-variable "enable_cluster_logging" {
-  type = bool
-  default = false
-  description = "Whether to enable cluster control plane logging"
+variable "enable_cluster_logs" {
+  description = "Flag to enable cluster logs"
+  type        = bool
+  default     = false
+}
+
+locals {
+  cluster_log_types = var.enable_cluster_logs ? ["audit", "api", "authenticator", "scheduler", "controllerManager"] : []
 }
